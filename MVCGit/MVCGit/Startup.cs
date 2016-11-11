3f833b3fe15bb895c3b38acd9bd7f44b96eff61a
Thomas <thomb@hotmail.dk>
2016-11-11 19:54:09 +0100

    Add ASP.NET MVC template

---
+++ MVCGit/MVCGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCGit.Startup))]
+namespace MVCGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
